15276524c7fb176230070f01fc97c40566a98d87
FranklineMisango <franklinemisango@gmail.com>
2025-07-25 16:23:39 +0200

    force C# update

--- Market_Making/MM_With_Informational_Disadvantage/gm-live-dashboard/run_dashboard.py
+++
@@ -1,6 +0,0 @@
-from src.dashboard.app import create_app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.config.suppress_callback_exceptions = True
-    app.run(debug=True, host='0.0.0.0', port=8050)
